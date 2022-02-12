6bd4cc934f0f11931dff43c777580eb8b3da7be9
Intetras <77957619+Intetras@users.noreply.github.com>
2022-02-12 12:08:33 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=0 icon="\uf075" color=#555555 block="kagabit-Energy"
+//% weight=0 icon="\uf0e7" color=#008000 block="kagabit-Energy"
 namespace kagabitenergy{
     //% blockID=comment block="コメント %v"
     export function comment(text:string):void{       
