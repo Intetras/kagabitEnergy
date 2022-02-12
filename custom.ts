8cfcea54b46ca8f9a8cbced6d2d54f3bc2ff0a97
Intetras <77957619+Intetras@users.noreply.github.com>
2022-02-12 12:54:37 +0900

    更新 custom.ts

--- custom.ts
+++ custom.ts
@@ -19,35 +19,15 @@ const stepVal = 0.00322;
 const gainVal = 85;
 const resistanceVal = 1;
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
-
 /**
  * Custom blocks
  */
 //% weight=50 icon="\uf0e7" color=#008000 block="kagabit-Energy"
 namespace kagabitenergy {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
+    // 電力を出力する
+    export function outputPower():number{
+        return 0;
     }
 
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
-    }
+    
 }
