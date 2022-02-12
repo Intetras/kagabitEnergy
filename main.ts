4c0de707cffdab05a670ce7902d36da8d0c5a730
Intetras <77957619+Intetras@users.noreply.github.com>
2022-02-12 12:10:20 +0900

    更新 pxt.json, main.ts, custom.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-//% weight=0 icon="\uf0e7" color=#008000 block="kagabit-Energy"
-namespace kagabitenergy{
-    //% blockID=comment block="コメント %v"
-    export function comment(text:string):void{       
-    }
-}
