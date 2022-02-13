db223282201e42969b44a0eb8574688a226da44b
Intetras <77957619+Intetras@users.noreply.github.com>
2022-02-13 12:57:30 +0900

    更新 custom.ts

--- custom.ts
+++ custom.ts
@@ -1,6 +1,8 @@
-
+// 1ステップの値
 const stepVal = 0.00322;
+// ゲイン 85
 const gainVal = 85;
+// 抵抗値 1
 const resistanceVal = 1;
 
 /**
@@ -10,27 +12,36 @@ const resistanceVal = 1;
 namespace kagabitenergy {
 
     //% group="基本"
-    //% blockId="outputPower"
-    //% block="電力"
-    export function outputPower():number{
-        
-        // 電圧の求め方
-        // P0で測定した値 p0Val
-        // ADコンバータ1ステップあたりの変化量 0.00322 stepVal
-        // 発電圧 bolt = p0Val * stepVal
-        
+    //% blockId="outputBolt"
+    //% block="電圧"
+    export function outputBolt():number{
+        // P0で測定した値　p0Val
         let p0Val = pins.analogReadPin(AnalogPin.P0);
-        let bolt = p0Val * stepVal;
+        // 3.3Vを1023で割った値が1ステップの値
+        return p0Val * stepVal;
+    }
 
-        // 電流の求め方
+    //% group="基本"
+    //% blockId="outputAmp"
+    //% block="電流"
+    export function outputAmp():number{
         // P1で測定した値　p1Val
         // この電圧はアンプで増幅された値である
         // ゲイン gainVal = 85
         // 抵抗値 resistanceVal = 1
         // 発電流 amp = p1Val * stepVal / gainVal / resistanceVal
-        
         let p1Val = pins.analogReadPin(AnalogPin.P1);
-        let amp = p1Val * stepVal / gainVal / resistanceVal;
+        return p1Val * stepVal / gainVal / resistanceVal;
+
+    }
+
+    //% group="基本"
+    //% blockId="outputPower"
+    //% block="電力"
+    export function outputPower():number{
+        
+        let bolt = outputBolt();
+        let amp = outputAmp();
 
         // 電力の求め方
         // 電圧 * 電流
